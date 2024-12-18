cd402535dfd953d4bdc9ac905cd18e36d4ab716e
Raphaeline <zlfkhr.akbar@gmail.com>
2024-12-18 12:03:57 +0700

    feat: add a crucial feature request

--- pages/people/index.js
+++ pages/people/index.js
@@ -3,23 +3,23 @@ import Head from "next/head";
 import SideBar from "../../components/People/SideBar";
 import MainSection from "../../components/People/MainSection";
 import AlumniSection from "../../components/People/AlumniSection";
-import BosSection from "../../components/People/BossSection";
+import BossSection from "../../components/People/BossSection";
 
 const index = () => {
-  return (
-    <div className="relative max-w-screen-xl mx-auto">
-      <Head>
-        <title>Assistant & Alumni</title>
-      </Head>
-      <Header />
-      <div className="">
-        {/* <SideBar /> */}
-        <BosSection />
-        <MainSection />
-        <AlumniSection />
-      </div>
-    </div>
-  );
+	return (
+		<div className="relative max-w-screen-xl mx-auto">
+			<Head>
+				<title>Assistant & Alumni</title>
+			</Head>
+			<Header />
+			<div className="">
+				{/* <SideBar /> */}
+				<BossSection />
+				<MainSection />
+				<AlumniSection />
+			</div>
+		</div>
+	);
 };
 
 export default index;
